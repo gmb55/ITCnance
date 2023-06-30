84f1f09336ad1dd14955621686ac939d8a13a6c5
Michał Bagrowski <m.bagrowski@fivedottwelve.com>
2023-06-30 20:57:02 +0200

    Initial Commit

---
+++ ITCnance/ITCnanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITCnanceApp.swift
+//  ITCnance
+//
+//  Created by Michał Bagrowski on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITCnanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
